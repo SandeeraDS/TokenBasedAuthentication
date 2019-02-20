f17be8de5b4729a826d3f92ebb24ad927dffa0d6
Dilusha Sandeera Jayampathi <32511955+SandeeraDS@users.noreply.github.com>
2019-02-20 22:19:50 +0530

    initial

---
+++ TokenBasedAuthentication/ApplicationOAuthProvider.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using TokenBasedAuthentication.Models;
+
+namespace TokenBasedAuthentication
+{
+    public class ApplicationOAuthProvider : OAuthAuthorizationServerProvider
+    {
+        //To Authenticate Client Device based given client id and secret code
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+
+            context.Validated();
+        }
+
+        //authenticate based on username and password
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            //have to check user name password from the databse
+            var userStore = new UserStore<ApplicationUser>(new ApplicationDbContext());
+            var manager = new UserManager<ApplicationUser>(userStore);
+
+            var user = await manager.FindAsync(context.UserName,context.Password);
+
+            if(user != null)
+            {
+                //claims related to the logged in user
+                //claims = statement about the logged in user
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                //now can add claims to this identity object
+
+                identity.AddClaim(new Claim("Username", user.UserName));
+                identity.AddClaim(new Claim("Email",user.Email));
+                identity.AddClaim(new Claim("FirstName",user.FirstName));
+                identity.AddClaim(new Claim("LastName",user.LastName));
+                identity.AddClaim(new Claim("LoggedOn",DateTime.Now.ToString()));
+                context.Validated(identity);
+
+            }
+            else
+            {
+                return;
+            }
+        }
+
+
+
+    }
+}
---
+++ TokenBasedAuthentication/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+[assembly: OwinStartup(typeof(TokenBasedAuthentication.Startup))]
+
+namespace TokenBasedAuthentication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCors(CorsOptions.AllowAll);
+
+            //apply token based authentication
+
+            //1st OAuth options
+            OAuthAuthorizationServerOptions option = new OAuthAuthorizationServerOptions
+            {
+                // /api/token post request with username & password
+                TokenEndpointPath = new PathString("/token"),
+                //apply auth provider
+                Provider = new ApplicationOAuthProvider(),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(60),
+                AllowInsecureHttp = true
+
+            };
+
+            //tell the application use above option
+            app.UseOAuthAuthorizationServer(option);
+            //specify  bear authentication
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
